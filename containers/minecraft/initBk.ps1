077c3b4ca7686b178294c28be082e01ef3203e48
Juan Pablo Morales Cruz <110570465+Kolozuz@users.noreply.github.com>
2025-12-24 00:37:37 -0500

    fix: correct indentation to match original

--- containers/minecraft/initBk.ps1
+++ containers/minecraft/initBk.ps1
@@ -11,80 +11,80 @@ $logFile = Join-Path $backupFolder "backup.log"
 # TODO: is this too much? it most certainly is!, should this be a module? maybe, but that'd mean more dependencies, so nose tu dime
 Function Write-Log
 {
-  param(
-    [string]$Message = '',
-    [ValidateSet('Info', 'Warning', 'Error', 'Success', 'Debug')]
-    [string]$Severity = 'Info'
-  )
-  Switch ($Severity)
-  {
-    'Warning' {
-      $FormattedMessage = "[WARNING] $Message"
-      Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.WarningForegroundColor -BackgroundColor $host.PrivateData.WarningBackgroundColor
-    }
-    'Error' {
-      $FormattedMessage = "[ERROR] $Message"
-      Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.ErrorForegroundColor -BackgroundColor $host.PrivateData.ErrorBackgroundColor
-      exit 1
-    }
-    'Debug' {
-      # Only log debug messages if debug stream shown
-      If ($DebugPreference -ine 'SilentlyContinue')
-      {
-        $FormattedMessage = "[DEBUG] $Message"
-        Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.DebugForegroundColor -BackgroundColor $host.PrivateData.DebugBackgroundColor
-      }
-    }
-    Default {
-      Write-Host $Message
-    }
-  }
+	param(
+		[string]$Message = '',
+		[ValidateSet('Info', 'Warning', 'Error', 'Success', 'Debug')]
+		[string]$Severity = 'Info'
+	)
+	Switch ($Severity)
+	{
+		'Warning' {
+			$FormattedMessage = "[WARNING] $Message"
+			Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.WarningForegroundColor -BackgroundColor $host.PrivateData.WarningBackgroundColor
+		}
+		'Error' {
+			$FormattedMessage = "[ERROR] $Message"
+			Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.ErrorForegroundColor -BackgroundColor $host.PrivateData.ErrorBackgroundColor
+			exit 1
+		}
+		'Debug' {
+			# Only log debug messages if debug stream shown
+			If ($DebugPreference -ine 'SilentlyContinue')
+			{
+				$FormattedMessage = "[DEBUG] $Message"
+				Write-Host $FormattedMessage -ForegroundColor $host.PrivateData.DebugForegroundColor -BackgroundColor $host.PrivateData.DebugBackgroundColor
+			}
+		}
+		Default {
+			Write-Host $Message
+		}
+	}
 }
 
 try
 {
-  Start-Transcript -Path $logFile -Append
+	Start-Transcript -Path $logFile -Append
 
-  Write-Log -Message "Creating backup folder..."
-  New-Item -Path $backupFolder -ItemType Directory -Force | Out-Null
-  Write-Log -Severity Success -Message "Backup folder '$backupFolder' successfully created!"
+	Write-Log -Message "Creating backup folder..."
+	New-Item -Path $backupFolder -ItemType Directory -Force | Out-Null
+	Write-Log -Severity Success -Message "Backup folder '$backupFolder' successfully created!"
 
-  Write-Log -Message "Init backup: $( Get-Date )"
-  Write-Log -Message "Backup origin: $dataPath"
-  Write-Log -Message "Backup destination: $backupFile"
+	Write-Log -Message "Init backup: $( Get-Date )"
+	Write-Log -Message "Backup origin: $dataPath"
+	Write-Log -Message "Backup destination: $backupFile"
 
-  if (!(Test-Path $dataPath))
-  {
-    Write-Log -Severity Error -Message "Data path '$dataPath' not found"
-  }
+	if (!(Test-Path $dataPath))
+	{
+		Write-Log -Severity Error -Message "Data path '$dataPath' not found"
+	}
 
-  Write-Log -Message "Compressing data..."
-  Compress-Archive -Path $dataPath -DestinationPath $backupFile -CompressionLevel Optimal
+	Write-Log -Message "Compressing data..."
+	Compress-Archive -Path $dataPath -DestinationPath $backupFile -CompressionLevel Optimal
 
-  $backupSize = (Get-Item $backupFile).Length
-  $backupSizeMB = [math]::Round($backupSize / 1MB, 2)
+	$backupSize = (Get-Item $backupFile).Length
+	$backupSizeMB = [math]::Round($backupSize / 1MB, 2)
 
-  Write-Log -Severity Success -Message "Backup complete: $backupFile ($backupSizeMB MB)"
+	Write-Log -Severity Success -Message "Backup complete: $backupFile ($backupSizeMB MB)"
 
-  # TODO: Make this a parameter, with default value 5
-  $backupsToKeep = 5
-  $oldBackups = Get-ChildItem $backupPath -Recurse -Filter "*.zip" |
-      Where-Object {
-        $_.LastWriteTime -lt (Get-Date).AddDays(-($backupsToKeep))
-      }
+	# TODO: Make this a parameter, with default value 5
+	$backupsToKeep = 5
+	$oldBackups = Get-ChildItem $backupPath -Recurse -Filter "*.zip" |
+		Where-Object {
+			$_.LastWriteTime -lt (Get-Date).AddDays(-($backupsToKeep))
+		}
 
-  if ($oldBackups.Count -gt 0)
-  {
-    Write-Log -Severity Warning -Message "Found $( $oldBackups.Count ) backups older than $backupsToKeep days, deleting..."
-    $oldBackups | Remove-Item -Force
-  }
+	if ($oldBackups.Count -gt 0)
+	{
+		Write-Log -Severity Warning -Message "Found $( $oldBackups.Count ) backups older than $backupsToKeep days, deleting..."
+		$oldBackups | Remove-Item -Force
+	}
 
 }
 catch
 {
-  Write-Log -Severity Error -Message "$( $_.Exception.Message )"
+	Write-Log -Severity Error -Message "$( $_.Exception.Message )"
 }
 finally
 {
-  Stop-Transcript
+	Stop-Transcript
 }
